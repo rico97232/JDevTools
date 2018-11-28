1586e9611c5afc2ded31b8dbea1054c2e759d327
ecrispin <ecrispin@telec75.easyvox.net>
2018-11-28 16:15:06 -0400

    Sup info cassandra addon

---
+++ src/org/easyvox/tools/jdbc/connection/Cassandra_Connection.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.easyvox.tools.jdbc.connection;
+
+/**
+ *
+ * @author ecrispin
+ */
+public class Cassandra_Connection {
+    
+}
